ffb3278f2c42e8939c55cf2ee762582a79c552af
sanketdalvi <AIURER9@amway.com>
2026-02-07 16:16:59 +0530

    Initial commit - Maven Spring Boot project

---
+++ src/test/java/com/src/ecom/EcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.src.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
